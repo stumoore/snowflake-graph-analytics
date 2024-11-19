7bede54c8b2847fd43cae9ed53401a4a1b118c87
zach-blumenfeld <zach.blumenfeld@neo4j.com>
2024-11-19 16:02:31 -0500

    Adding Cleaned Up Patient Journey Example With Filters

--- patient-journey/PJ_DEMO_STEP3_ALGOS.sql
+++ patient-journey/PJ_DEMO_STEP3_ALGOS.sql
@@ -24,10 +24,14 @@ GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA PATIENT_DB.PUBLIC TO ROLE gds_ro
 GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA PATIENT_DB.PUBLIC TO ROLE accountadmin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PATIENT_DB.PUBLIC TO ROLE accountadmin;
 
+-- Spin up Neo4j
 CALL neo4j_graph_analytics.gds.create_session('CPU_X64_M');
 
-SELECT neo4j_graph_analytics.gds.graph_drop('enc_reason_graph', { 'failIfMissing': false });
+-- show the various functions available
+-- SHOW USER FUNCTIONS IN APPLICATION neo4j_graph_data_science;
 
+-- drop graph if already exists.
+SELECT neo4j_graph_analytics.gds.graph_drop('enc_graph', { 'failIfMissing': false });
 
 -- ///////////////////////////////////////////////////////////
 -- 2) PATIENT SIMILARITY
@@ -35,110 +39,102 @@ SELECT neo4j_graph_analytics.gds.graph_drop('enc_reason_graph', { 'failIfMissing
 USE DATABASE PATIENT_DB;
 USE SCHEMA PATIENT_DB.PUBLIC;
 
-SELECT neo4j_graph_analytics.gds.graph_project('enc_reason_graph', {
-    'nodeTable':         'patient_db.public.nodes',
+-- create graph projection
+SELECT neo4j_graph_analytics.gds.graph_project('enc_graph', {
+    'nodeTable': 'patient_db.public.nodes',
     'relationshipTable': 'patient_db.public.relationships',
-    'readConcurrency':   28
+    'readConcurrency': 28
 });
 
-
-SELECT neo4j_graph_analytics.gds.node_similarity('enc_reason_graph', {
-    'mutateRelationshipType': 'SIMILAR_ENC_REASONS_TO',
-    'mutateProperty':         'similarity',
-    'concurrency':            28
+-- run node similarity to find similar patients by encounter procedures
+SELECT neo4j_graph_analytics.gds.node_similarity('enc_graph', {
+    'mutateRelationshipType': 'SIMILAR_PROCEDURES_TO',
+    'mutateProperty': 'similarity',
+    'concurrency': 28
 });
 
-SELECT neo4j_graph_analytics.gds.write_relationships('enc_reason_graph', {
-    'relationshipType':     'SIMILAR_ENC_REASONS_TO',
-    'relationshipProperty': 'similarity',
-    'table':                'patient_db.public.SIMILAR_ENC_REASONS_TO'
+-- (OPTIONAL) write similarity relationships back to a table and view
+SELECT neo4j_graph_analytics.gds.write_relationships('enc_graph', {
+'relationshipType': 'SIMILAR_PROCEDURES_TO',
+'relationshipProperty': 'similarity',
+'table': 'patient_db.public.SIMILAR_PROCEDURES_TO'
 });
-SELECT * FROM SIMILAR_ENC_REASONS_TO;
+SELECT * FROM SIMILAR_PROCEDURES_TO;
 
 
-SELECT neo4j_graph_data_science.gds.degree_centrality('enc_reason_graph', {
-    'mutateProperty': 'degree'
-});
-SELECT neo4j_graph_data_science.gds.write_nodeproperties('enc_reason_graph',
- {'nodeProperties': ['degree'], 'table': 'patient_db.public.degree'});
-
- 
 -- ///////////////////////////////////////////////////////////
--- 3) PATIENT COHORTS (LEIDEN COMMUNITIES)
+-- 3) PATIENT COHORTS (LOUVAIN COMMUNITIES)
 -- ///////////////////////////////////////////////////////////
-SHOW USER FUNCTIONS IN APPLICATION neo4j_graph_data_science;
 
-SELECT neo4j_graph_analytics.gds.louvain('enc_reason_graph', {
-    'relationshipTypes': ['SIMILAR_ENC_REASONS_TO'],
+-- run louvain community detection
+SELECT neo4j_graph_analytics.gds.louvain('enc_graph', {
+    'relationshipTypes': ['SIMILAR_PROCEDURES_TO'],
     'relationshipWeightProperty': 'similarity',
     'mutateProperty': 'cohort'
 });
 
-SELECT neo4j_graph_analytics.gds.write_nodeproperties('enc_reason_graph',
- {'nodeProperties': ['cohort'], 'table': 'patient_db.public.cohorts'});
+-- write community ids, a.k.a. "cohorts" to a table & view
+SELECT neo4j_graph_analytics.gds.write_nodeproperties('enc_graph',
+           {'nodeProperties': ['cohort'], 'table': 'patient_db.public.cohorts'});
 SELECT * FROM COHORTS;
 
--- create patient cohort view
+-- combine cohorts with  patient data in patient_cohorts view
 CREATE OR REPLACE VIEW PATIENT_COHORTS AS
-    SELECT  COHORTS.COHORT, PATIENTS.*
-    FROM PATIENT_NODE_MAPPING
-    JOIN COHORTS ON PATIENT_NODE_MAPPING.NODEID = COHORTS.NODEID 
-    JOIN PATIENTS ON PATIENT_NODE_MAPPING.ID = PATIENTS.ID
-    ORDER BY COHORT DESC;
+SELECT  COHORTS.COHORT, PATIENTS.*
+FROM PATIENT_NODE_MAPPING
+         JOIN COHORTS ON PATIENT_NODE_MAPPING.NODEID = COHORTS.NODEID
+         JOIN PATIENTS ON PATIENT_NODE_MAPPING.ID = PATIENTS.ID
+ORDER BY COHORT DESC;
 SELECT * FROM PATIENT_COHORTS;
 
-CREATE OR REPLACE VIEW ENCOUNTER_COHORTS AS
-    SELECT  PATIENT_COHORTS.COHORT, ENCOUNTERS.*
-    FROM ENCOUNTERS
-    JOIN PATIENT_COHORTS ON ENCOUNTERS.PATIENT = PATIENT_COHORTS.ID;
-SELECT * FROM ENCOUNTER_COHORTS;
-
-CREATE OR REPLACE VIEW ENCOUNTER_COHORT_PATHS AS
-SELECT COHORT, ID, START_TIME, PATIENT, REASONCODE,
-FIRST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) SOURCE_ENC,
-LAST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) TARGET_ENC
-FROM ENCOUNTER_COHORTS
-ORDER BY COHORT, PATIENT, START_TIME;
-SELECT * FROM ENCOUNTER_COHORT_PATHS;
-
-CREATE OR REPLACE VIEW ENCOUNTER_COHORT_SANKY AS
-SELECT COHORT, SOURCE_ENC, TARGET_ENC, COUNT(*) AS FREQ
-FROM ENCOUNTER_COHORT_PATHS
-WHERE (SOURCE_ENC IS NOT NULL) AND (TARGET_ENC IS NOT NULL)
-GROUP BY COHORT, SOURCE_ENC, TARGET_ENC
-ORDER BY COHORT, FREQ DESC;
-SELECT * FROM ENCOUNTER_COHORT_SANKY;
+-- view cohort patient counts
+SELECT COHORT, COUNT(*) AS CNT FROM PATIENT_COHORTS GROUP BY COHORT ORDER BY CNT DESC;
 
 -- ///////////////////////////////////////////////////////////
--- 4) CLEANUP
+-- 4) CLEANUP NEO4J
 -- ///////////////////////////////////////////////////////////
+-- turn off Neo4j and spins down compute pool
 CALL neo4j_graph_analytics.gds.stop_session();
 
 -- ///////////////////////////////////////////////////////////
 -- 5) VIEWS FOR ANALYSIS
 -- ///////////////////////////////////////////////////////////
+-- below we create some more helpful views for further analysis
+    -- mainly a view for creating sankey charts to visualize patient journeys by cohort
+-- We will filter out some ubiquitous procedures for easier readability. Namely:
+    -- CODE=274804006 (Evaluation of uterine fundal height)
+    -- CODE=225158009 (Auscultation of the fetal heart)
+-- The below commented out query was used to identify these  ubiquitous procedures
+/*SELECT COHORT, CODE, DESCRIPTION, CNT, PERCENT_CNT
+FROM (
+    SELECT COHORT, CODE, DESCRIPTION, COUNT(*) AS CNT, RATIO_TO_REPORT(CNT) OVER (PARTITION BY COHORT) AS PERCENT_CNT
+    FROM PROCEDURE_COHORTS
+    GROUP BY COHORT, CODE, DESCRIPTION
+)
+WHERE percent_cnt > 0.1
+ORDER BY COHORT, CNT DESC;*/
 
-CREATE OR REPLACE VIEW ENCOUNTER_PATHS AS
-SELECT ID, START_TIME, PATIENT, REASONCODE,
-FIRST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) SOURCE_ENC,
-LAST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) TARGET_ENC
-FROM ENCOUNTERS
-ORDER BY PATIENT, START_TIME;
-SELECT * FROM ENCOUNTER_PATHS;
+-- create procedure cohort view filtering out ubiquitous procedures
+CREATE OR REPLACE VIEW PROCEDURE_COHORTS AS
+SELECT  PATIENT_COHORTS.COHORT, PROCEDURES.*
+FROM PROCEDURES
+         JOIN PATIENT_COHORTS ON PROCEDURES.PATIENT = PATIENT_COHORTS.ID
+WHERE PROCEDURES.REASONCODE=72892002 //Pregnancy Encounters;
+AND PROCEDURES.CODE NOT IN (274804006, 225158009); // Evaluation of uterine fundal height & Auscultation of the fetal heart
+SELECT * FROM PROCEDURE_COHORTS;
 
-CREATE OR REPLACE VIEW ENCOUNTER_SANKY AS
-SELECT SOURCE_ENC, TARGET_ENC, COUNT(*) AS FREQ
-FROM ENCOUNTER_PATHS
-WHERE (SOURCE_ENC IS NOT NULL) AND (TARGET_ENC IS NOT NULL)
-GROUP BY SOURCE_ENC, TARGET_ENC
-ORDER BY FREQ DESC;
-SELECT * FROM ENCOUNTER_SANKY;
+-- create sankey view (TODO: The below can likely be combined into one query)
+CREATE OR REPLACE VIEW PROCEDURE_COHORT_PATHS AS
+SELECT COHORT, PATIENT, ENCOUNTER, START_TIME, CODE,
+    FIRST_VALUE(CODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME, ENCOUNTER ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) SOURCE_PROC,
+    LAST_VALUE(CODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME, ENCOUNTER  ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) TARGET_PROC
+FROM PROCEDURE_COHORTS
+ORDER BY COHORT, PATIENT, START_TIME, ENCOUNTER;
+SELECT * FROM PROCEDURE_COHORT_PATHS;
 
-/* --- this one shot isn't working
-SELECT COUNT(*) AS FREQ,
-FIRST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) SOURCE_ENC,
-LAST_VALUE(REASONCODE) OVER (PARTITION BY PATIENT ORDER BY START_TIME ROWS BETWEEN 1 PRECEDING AND 0 PRECEDING) TARGET_ENC
-FROM ENCOUNTERS
-WHERE REASONCODE IS NOT NULL
-GROUP BY SOURCE_ENC, TARGET_ENC
-ORDER BY FREQ DESC;*/
+CREATE OR REPLACE VIEW PROCEDURE_COHORT_SANKEY AS
+SELECT COHORT, SOURCE_PROC, TARGET_PROC, COUNT(*) AS FREQ, RATIO_TO_REPORT(FREQ) OVER (PARTITION BY COHORT) AS PERCENT_FREQ
+FROM PROCEDURE_COHORT_PATHS
+GROUP BY COHORT, SOURCE_PROC, TARGET_PROC
+ORDER BY COHORT, FREQ DESC;
+SELECT * FROM PROCEDURE_COHORT_SANKEY;
